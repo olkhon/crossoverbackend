88b46e12eb84b1f75e5a2dd655e225e2e7af5386
olkhon <alexander.stapel@gmail.com>
2021-02-26 10:06:12 +0100

    second up thing

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+// for json stuff
+app.use(bodyParser.json());
+
+const userRoutes = require('./Routes/user');
+
+
+const port = 3000;
+
+app.use('/user', userRoutes);
+
+
+
+const port = 3000;
+
+
+
+
+app.listen(port, () => console.log(`Server running on port: ${port} `));
