4ada59a69e712827de4e896ea9dd11bfc9154ed8
olkhon <alexander.stapel@gmail.com>
2021-02-26 12:26:45 +0100

    try catch index.js

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const express = require('express');
-const app = express();
+try {
+    const express = require('express');
+    const app = express();
 
-const dotenv = require('dotenv');
-dotenv.config();
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-// for json stuff
-// app.use(bodyParser.json());
-
-app.use(express.json())
-
-const userRoutes = require('./Routes/user');
-const messagesRoutes = require("./Routes/messages");
-
-app.use('/user', userRoutes);
-app.use("/messages", messagesRoutes);
-
-const {PORT} = process.env;
+    const dotenv = require('dotenv');
+    dotenv.config();
+    var bodyParser = require('body-parser');
+    app.use(bodyParser.urlencoded({ extended: true }));
+    // for json stuff
+    // app.use(bodyParser.json());
 
 
+    app.use(express.json())
+
+    const userRoutes = require('./Routes/user');
+    const messagesRoutes = require("./Routes/messages");
+
+    app.use('/user', userRoutes);
+    app.use("/messages", messagesRoutes);
+
+    const {PORT} = process.env;
 
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT} `));
+
+
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT} `));
+} catch (error) {
+    console.log(error);
+}
+
